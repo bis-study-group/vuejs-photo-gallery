6fc17dea07d7d289b3f138b5cb3a647725876f7b
*w4ohelX9fa <taira.iwasawa@ari-jp.com>
2021-04-07 17:51:15 +0900

    完成

--- app.js
+++ app.js
@@ -3,29 +3,43 @@ Vue.component('gallery', {
     <div class="gallery">
       <h2 class="gallery-title">{{ title }}</h2>
       <!-- Insert code here... -->
+      <img  class="mainImage" :src="url">
+      <ul class="thumbnailList">
+        <li class="thumbnailList-item" v-for="item in items">
+          <img :src="'images/' + item.main" v-on:click="updateUrl">
+        </li>
+      </ul>
     </div>
   `,
   /* Insert code here... */
+  props: {
+    title: String,
+    items: Array
+  },
+  data() {
+    return {
+      url: 'images/' + this.items[0].main
+    }
+  },
+  methods: {
+    updateUrl: function(event) {
+      this.url = event.target.src
+    }
+  }
 });
 
 new Vue({
   el: '#app',
   data: {
     photos1: [
-      { id: 1, main: 'image_1.jpeg', thumbnail: 'thumbnail_1.jpeg' },
-      { id: 2, main: 'image_2.jpeg', thumbnail: 'thumbnail_2.jpeg' },
-      { id: 3, main: 'image_3.jpeg', thumbnail: 'thumbnail_3.jpeg' }
+      { main: 'image_1.jpeg', thumbnail: 'thumbnail_1.jpeg' },
+      { main: 'image_2.jpeg', thumbnail: 'thumbnail_2.jpeg' },
+      { main: 'image_3.jpeg', thumbnail: 'thumbnail_3.jpeg' }
     ],
     photos2: [
-      { id: 4, main: 'image_4.jpeg', thumbnail: 'thumbnail_4.jpeg' },
-      { id: 5, main: 'image_5.jpeg', thumbnail: 'thumbnail_5.jpeg' },
-      { id: 6, main: 'image_6.jpeg', thumbnail: 'thumbnail_6.jpeg' }
-    ],
-    url: 'images/image_1.jpeg'
-  },
-  methods: {
-    handleImage: function(event) {
-      this.url = event.target.src
-    }
+      { main: 'image_4.jpeg', thumbnail: 'thumbnail_4.jpeg' },
+      { main: 'image_5.jpeg', thumbnail: 'thumbnail_5.jpeg' },
+      { main: 'image_6.jpeg', thumbnail: 'thumbnail_6.jpeg' }
+    ]
   },
 });
